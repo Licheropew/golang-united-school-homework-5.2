7ac54bf6824e05fe76aa2b041f73e14b28be7540
unknown <anton.aksiutsin@gmail.com>
2022-05-20 08:13:50 +0300

    Add Get, Put, Keys functions

--- cache.go
+++ cache.go
@@ -3,21 +3,42 @@ package cache
 import "time"
 
 type Cache struct {
+	cache map[string]customCache
+}
+
+type customCache struct {
+	value    string
+	deadline time.Time
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	now := time.Now()
+	for k, v := range c.cache {
+		if k == key && v.deadline.Before(now) {
+			return v.value, true
+		}
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.cache[key] = customCache{value: value, deadline: time.Unix(1<<63-1, 0)}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	result := []string{}
+	now := time.Now()
+	for k, v := range c.cache {
+		if v.deadline.Before(now) {
+			result = append(result, k)
+		}
+	}
+	return result
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
 }
